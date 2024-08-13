61a3f0d33abacf18d72a77ec7fc88515922f54ef
Sagnik Mukherjee <mukherjee.sagnik17@gmail.com>
2024-08-13 12:33:44 +0530

    completed

--- app/src/main/java/com/itssagnikmukherjee/dicegamemultiplayer/MainPreview.kt
+++ app/src/main/java/com/itssagnikmukherjee/dicegamemultiplayer/MainPreview.kt
@@ -2,16 +2,22 @@ package com.itssagnikmukherjee.dicegamemultiplayer
 
 import android.annotation.SuppressLint
 import android.provider.MediaStore.Images
+import android.widget.ImageButton
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -24,10 +30,13 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.motionEventSpy
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.launch
 import kotlin.random.Random
 
@@ -51,22 +60,39 @@ fun MainPreview() {
         }
         currentImg = Random.nextInt(6) + 1
     }
-
+    if (player1Score>=20 || player2Score>=20) {
+        if (player1Score>player2Score) {
+            Column(
+                modifier = Modifier.fillMaxSize().padding(20.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                Image(painter = painterResource(id = R.drawable.batman), contentDescription = "",modifier = Modifier.size(100.dp))
+                Spacer(modifier = Modifier.height(30.dp))
+                Text(text = "Batman Wins !", fontSize = 40.sp, fontWeight = FontWeight.Bold)
+                Text(text = "$player1Score", fontSize = 40.sp, fontWeight = FontWeight.Bold)
+            }
+        } else {
+            Column (
+                modifier = Modifier.fillMaxSize().padding(20.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                Image(painter = painterResource(id = R.drawable.spiderman), contentDescription = "", modifier = Modifier.size(100.dp))
+                Spacer(modifier = Modifier.height(30.dp))
+            Text(text = "Spiderman Wins !", fontSize = 30.sp,fontWeight = FontWeight.Bold)
+                Text(text = "$player2Score", fontSize = 40.sp, fontWeight = FontWeight.Bold)
+            }
+        }
+    }else{
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Row(
-            modifier = Modifier.fillMaxWidth(0.8f),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            Text(text = "Player 1 score : $player1Score")
-            Text(text = "Player 2 score : $player2Score")
-        }
-        Text(text = if (player1turn) "Player 1 turn" else "Player 2 turn")
+        Text(text = if (player1turn) "Batman" else "Spiderman", fontSize = 40.sp, fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.height(30.dp))
 
-        // Rotate the image
         Image(
             painter = painterResource(getImageList()[currentImg - 1]),
             contentDescription = "",
@@ -74,40 +100,51 @@ fun MainPreview() {
                 .rotate(rotation.value)
                 .size(300.dp)
         )
-        if (player1Score>=20 || player2Score>=20) {
-            if (player1Score>player2Score) {
-                Text(text = "Player 1 wins")
-            } else {
-                Text(text = "Player 2 wins")
-            }
-        }else {
-            Row {
-                Button(
-                    onClick = {
+            Row(
+                modifier = Modifier.fillMaxWidth(0.6f),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ){
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ){
+                    Button(onClick = {
                         coroutineScope.launch {
                             rollDice()
                             player1Score += currentImg
                             player1turn = false
-                        }
-                    },
-                    enabled = player1turn
-                ) {
-                    Text(text = "Player 1")
+                        } }, modifier = Modifier.size(100.dp),enabled = player1turn,
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color(0xFFe67e22)
+                        )
+
+                    ) {
+                        Image(painter = painterResource(id = R.drawable.batman), contentDescription = "")
+                    }
+
+                    Text(text = "$player1Score", fontSize = 70.sp, fontWeight = FontWeight.Bold)
                 }
-                Button(
-                    onClick = {
+                Column (
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ){
+                    Button(onClick = {
                         coroutineScope.launch {
                             rollDice()
                             player2Score += currentImg
                             player1turn = true
                         }
-                    },
-                    enabled = !player1turn
-                ) {
-                    Text(text = "Player 2")
+                    }, modifier = Modifier.size(100.dp), enabled = !player1turn,
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color(0xFFe67e22)
+                        )
+                    ) {
+                        Image(painter = painterResource(id = R.drawable.spiderman), contentDescription = "")
+                    }
+                    Text(text = "$player2Score", fontSize = 70.sp, fontWeight = FontWeight.Bold)
                 }
-            }
         }
+    }
     }
 }
 
